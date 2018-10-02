11547232f6f46412fad769a64f3e3b5656aac100
ashuag007 <agarwal12345ashish@gmail.com>
2018-10-02 20:01:52 +0530

    Added hello world program in c++

---
+++ ashuag007/hello .cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ void printHello()
+{ 
+    cout<<"Hello World!!";
+} 
+ 
+int main()
+{
+    printHello();
+	return 0;
+}
